cbfd4dfb45361bb592d7da88503af9ada6f11241
Yeondam Park <giyen9088@gmail.com>
2018-04-30 23:25:36 -0700

    finishing register api

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
 
 // Load User model
 const User = require("../../models/User");
@@ -18,11 +20,27 @@ router.post("/register", (req, res) => {
     if (user) {
       return res.status(400).json({ email: "Email is already exists" });
     } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: "200",
+        r: "pg",
+        d: "mm"
+      });
       const newUser = new User({
         name: req.body.name,
         email: req.body.email,
         avatar: avatar,
         password: req.body.password
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
       });
     }
   });

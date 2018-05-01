be02fc29b144416f1ddc4951912b7a1b46bd598f
Yeondam Park <giyen9088@gmail.com>
2018-04-30 23:17:15 -0700

    working on register

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,31 @@
 const express = require("express");
 const router = express.Router();
 
+// Load User model
+const User = require("../../models/User");
+
+// @route GET api/users/test
+// @desc Tests users route
+// @access Public
 router.get("/test", (req, res) => res.json({ msg: "Users work!" }));
+
+// @route GET api/users/register
+// @desc Register user
+// @access Public
+
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: "Email is already exists" });
+    } else {
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar: avatar,
+        password: req.body.password
+      });
+    }
+  });
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
@@ -7,6 +8,11 @@ const port = process.env.PORT || 5000;
 
 /* Initialize the App */
 const app = express();
+
+/* Body Parser middleware */
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 /* DB Config */
 const db = require("./config/keys").mongoURI;
 /* Connect to MongoDB  */

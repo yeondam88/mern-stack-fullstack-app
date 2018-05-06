c60b80cfbb048d94986fb5f45686170ee509699b
Yeondam Park <giyen9088@gmail.com>
2018-05-06 14:20:12 -0700

    setting validator for profile

---
+++ validation/profile.js
@@ -0,0 +1,122 @@
+const Validator = require("validator");
+const isEmpty = require("./is-empty");
+
+module.exports = function validateProfileInput(data) {
+  let errors = {};
+
+  // Make sure name is empty string
+  data.handle = !isEmpty(data.handle) ? data.handle : "";
+  data.status = !isEmpty(data.status) ? data.status : "";
+  data.skills = !isEmpty(data.skills) ? data.skills : "";
+
+  if (!Validator.isLength(data.handle, { min: 2, max: 40 })) {
+    errors.handle = "Handle needs to between 2 and 4 characters";
+  }
+
+  if (!Validator.isEmpty(data.handle)) {
+    errors.handle = "Profile handle is required.";
+  }
+
+  if (!Validator.isEmpty(data.status)) {
+    errors.status = "Status field is required.";
+  }
+
+  if (!Validator.isEmpty(data.skills)) {
+    errors.skills = "Status field is required.";
+  }
+
+  if (!isEmpty(data.website)) {
+    if (!Validator.isURL(data.website)) {
+      errors.website = "Not a valid URL";
+    }
+  }
+
+  if (!isEmpty(data.youtube)) {
+    if (!Validator.isURL(data.youtube)) {
+      errors.youtube = "Not a valid URL";
+    }
+  }
+
+  if (!isEmpty(data.facebook)) {
+    if (!Validator.isURL(data.facebook)) {
+      errors.facebook = "Not a valid URL";
+    }
+  }
+
+  if (!isEmpty(data.twitter)) {
+    if (!Validator.isURL(data.twitter)) {
+      errors.twitter = "Not a valid URL";
+    }
+  }
+
+  if (!isEmpty(data.linkedin)) {
+    if (!Validator.isURL(data.linkedin)) {
+      errors.linkedin = "Not a valid URL";
+    }
+  }
+
+  if (!isEmpty(data.instagram)) {
+    if (!Validator.isURL(data.instagram)) {
+      errors.instagram = "Not a valid URL";
+    }
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
+const Validator = require("validator");
+const isEmpty = require("./is-empty");
+
+module.exports = function validateLoginInput(data) {
+  let errors = {};
+
+  // Make sure name is empty string
+  data.email = !isEmpty(data.email) ? data.email : "";
+  data.password = !isEmpty(data.password) ? data.password : "";
+
+  if (!Validator.isEmail(data.email)) {
+    errors.email = "Email is inValid";
+  }
+
+  if (Validator.isEmpty(data.email)) {
+    errors.email = "Email field is required";
+  }
+
+  if (Validator.isEmpty(data.password)) {
+    errors.password = "Password field is required";
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
+const Validator = require("validator");
+const isEmpty = require("./is-empty");
+
+module.exports = function validateLoginInput(data) {
+  let errors = {};
+
+  // Make sure name is empty string
+  data.email = !isEmpty(data.email) ? data.email : "";
+  data.password = !isEmpty(data.password) ? data.password : "";
+
+  if (!Validator.isEmail(data.email)) {
+    errors.email = "Email is inValid";
+  }
+
+  if (Validator.isEmpty(data.email)) {
+    errors.email = "Email field is required";
+  }
+
+  if (Validator.isEmpty(data.password)) {
+    errors.password = "Password field is required";
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};

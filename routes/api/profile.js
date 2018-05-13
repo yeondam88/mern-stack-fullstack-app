d9900fd69d2670ae2c53de80be1d0c2b0d31c2b9
Yeondam Park <giyen9088@gmail.com>
2018-05-12 22:06:32 -0700

    remove experience api route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -259,4 +259,29 @@ router.post('/education', passport.authenticate('jwt', {
   });
 })
 
+// @route DELETE api/profile/experience/:exp_id
+// @desc  Delete experience from profile
+// @access PRIVATE
+
+router.post('/experience/:exp_id', passport.authenticate('jwt', {
+  session: false
+}), (req, res) => {
+
+  Profile.findOne({
+      user: req.user.id
+    }).then(profile => {
+      // Get remove index
+      const removeIndex = profile.experience
+        .map(item => item.id)
+        .indexOf(req.params.exp_id)
+
+      // Splice out of array
+      profile.experience.splice(removeIndex, 1);
+
+      // Save
+      profile.save().then(profile => res.json(profile));
+    })
+    .catch(err => res.status(404).json(err));
+})
+
 module.exports = router;

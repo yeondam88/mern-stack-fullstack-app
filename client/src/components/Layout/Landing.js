206415e10c9b11ce1f76a579d557f4667575dd78
Yeondam Park <giyen9088@gmail.com>
2018-05-18 21:14:58 -0700

    working on landing page

--- client/src/components/Layout/Landing.js
+++ client/src/components/Layout/Landing.js
@@ -4,7 +4,9 @@ class Landing extends Component {
   render() {
     return (
       <div>
-        
+        <h1>Looking for connect with other developer?</h1>
+        <h4>Please join us today.</h4>
+        <button>Sign up now</button>
       </div>
     );
   }

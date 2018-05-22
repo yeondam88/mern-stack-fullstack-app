a8bd6dd69544cfd0899e0add1c35b945eee62779
Yeondam Park <giyen9088@gmail.com>
2018-05-21 23:33:09 -0700

    still working on landing

--- client/src/components/Layout/Landing.js
+++ client/src/components/Layout/Landing.js
@@ -1,38 +1,91 @@
-import React, { Component } from "react";
-import CodeImg from "../../images/code-icon.png";
+import React, { Component, Fragment } from "react";
+import CodeImg from "../../images/lighthouse.jpg";
+import Logo from "../../images/code-icon.png";
+import icon1 from "../../images/offices.svg";
+import icon2 from "../../images/process.svg";
+import icon3 from "../../images/coding.svg";
 
 class Landing extends Component {
   render() {
     return (
-      <div className="is-hero section">
-        <div className="container">
-          <div className="columns is-vcentered">
-            <div className="column is-6 is-5-fullhd is-offset-1-fullhd">
-              <div className="section-header">
-                <h1 className="title is-spaced">Devconnector</h1>
-                <h2 className="subtitle is-3">Socail Network for Developer</h2>
-                <div className="field is-grouped">
-                  <div className="control">
-                    <a href="#" className="button is-primary is-medium">
-                      Register
-                    </a>
-                  </div>
-                  <div className="control">
-                    <a href="#" className="button is-primary is-medium">
-                      Sign in
-                    </a>
+      <Fragment>
+        <section className="is-hero section">
+          <div className="container">
+            <div className="columns is-vcentered">
+              <div className="column is-6 is-5-fullhd is-offset-1-fullhd">
+                <div className="section-header">
+                  <h1 className="title is-spaced">Devconnector</h1>
+                  <h2 className="subtitle is-3">
+                    Socail Network for Developer
+                  </h2>
+                  <div className="field is-grouped">
+                    <div className="control">
+                      <a href="#" className="button is-primary is-medium">
+                        Register
+                      </a>
+                    </div>
+                    <div className="control">
+                      <a href="#" className="button is-primary is-medium">
+                        Sign in
+                      </a>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-            <div className="column is-6">
-              <div className="section-media">
-                <img src={CodeImg} alt="code" />
+              <div className="column is-6">
+                <div className="section-media">
+                  <img src={CodeImg} alt="code" />
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </div>
+        </section>
+        <section className="is-medium has-background is-usp">
+          <div className="container">
+            <div className="columns">
+              <div className="column is-4">
+                <div className="card is-usp">
+                  <img src={icon1} alt="logo" width="50px" />
+                  <hr className="spacer is-1" />
+                  <h3 className="title is-4 is-spaced">Connection</h3>
+                  <p className="subtitle is-5">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. In
+                    dicta quaerat est? Dolore, recusandae voluptatum ad nihil
+                    quod nesciunt nulla quidem magni dignissimos laudantium
+                    alias error dolorum obcaecati est eius.
+                  </p>
+                </div>
+              </div>
+              <div className="column is-4">
+                <div className="card is-usp">
+                  <img src={icon2} alt="logo" width="50px" />
+                  <hr className="spacer is-1" />
+                  <h3 className="title is-4 is-spaced">Connection</h3>
+                  <p className="subtitle is-5">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. In
+                    dicta quaerat est? Dolore, recusandae voluptatum ad nihil
+                    quod nesciunt nulla quidem magni dignissimos laudantium
+                    alias error dolorum obcaecati est eius.
+                  </p>
+                </div>
+              </div>
+              <div className="column is-4">
+                <div className="card is-usp">
+                  <img src={icon3} alt="logo" width="50px" />
+                  <hr className="spacer is-1" />
+                  <h3 className="title is-4 is-spaced">Connection</h3>
+                  <p className="subtitle is-5">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. In
+                    dicta quaerat est? Dolore, recusandae voluptatum ad nihil
+                    quod nesciunt nulla quidem magni dignissimos laudantium
+                    alias error dolorum obcaecati est eius.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </Fragment>
     );
   }
 }

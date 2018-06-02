795d68c35b4474dcaf0ce8900499121b6d5d725b
Yeondam Park <giyen9088@gmail.com>
2018-06-01 22:47:11 -0700

    displaying each profile

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -20,6 +20,19 @@ export const getCurrentProfile = () => dispatch => {
   }))
 }
 
+// Get profile by handle
+export const getProfileByHandle = (handle) => dispatch => {
+  dispatch(setProfileLoading());
+  axios.get(`/api/profile/handle/${handle}`).then((res) => dispatch({
+    type: GET_PROFILE,
+    payload: res.data
+  })).catch(err => dispatch({
+    type: GET_PROFILE,
+    payload: {}
+  }))
+
+}
+
 // Get all profiles
 export const getProfiles = () => dispatch => {
   dispatch(setProfileLoading());
---
+++ client/src/components/profile/ProfileAbout.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class ProfileAbout extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Profile Head</h1>
+      </div>
+    );
+  }
+}
+
+export default ProfileAbout;

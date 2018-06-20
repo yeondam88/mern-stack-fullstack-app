37193309f2fd15193572294a17532a6bec633162
Yeondam Park <giyen9088@gmail.com>
2018-06-19 21:02:19 -0700

    touch up with styles

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -31,68 +31,65 @@ class PostItem extends Component {
     const { post, auth, showActions } = this.props;
 
     return (
-      <article className="media">
-        <figure className="media-left">
-          <p className="image is-64x64">
-            <img src={post.avatar} />{" "}
-          </p>{" "}
-        </figure>{" "}
-        <div className="media-content">
-          <div className="content">
-            <p>
-              <strong> {post.name} </strong> <small>@{post.name}</small>{" "}
-              <small> {post.date} </small> <br /> {post.text}{" "}
+      <div className="post-container">
+        <article className="media">
+          <figure className="media-left">
+            <p className="image is-64x64">
+              <img src={post.avatar} />{" "}
             </p>{" "}
-          </div>{" "}
-          {showActions ? (
-            <nav className="level is-mobile">
-              <div className="level-left">
-                <div className="level-item">
-                  <span className="icon is-small">
-                    <Link to={`/post/${post._id}`}>
-                      <i className="fas fa-reply" />
-                    </Link>{" "}
-                  </span>{" "}
+          </figure>{" "}
+          <div className="media-content">
+            <div className="content">
+              <p>
+                <strong> {post.name} </strong> <small>@{post.name}</small>{" "}
+                <small> {post.date} </small> <br /> {post.text}{" "}
+              </p>{" "}
+            </div>{" "}
+            {showActions ? (
+              <nav className="level is-mobile">
+                <div className="level-left">
+                  <div className="level-item">
+                    <span className="icon is-small">
+                      <Link to={`/post/${post._id}`}>
+                        <i className="fas fa-reply" />
+                      </Link>{" "}
+                    </span>{" "}
+                  </div>{" "}
+                  <a className="level-item">
+                    <span
+                      onClick={this.onLikeClick.bind(this, post._id)}
+                      className="icon is-small"
+                    >
+                      <i
+                        className={classnames("fas fa-thumbs-up", {
+                          "has-text-success": this.findUserLike(post.likes)
+                        })}
+                      />
+                      {post.likes.length}
+                    </span>
+                  </a>{" "}
+                  <a className="level-item">
+                    <span
+                      onClick={this.onUnLikeClick.bind(this, post._id)}
+                      className="icon is-small"
+                    >
+                      <i className="fas fa-thumbs-down" />
+                    </span>{" "}
+                  </a>{" "}
                 </div>{" "}
-                <a className="level-item">
-                  <span className="icon is-small">
-                    <i className="fas fa-retweet" />
-                  </span>{" "}
-                </a>{" "}
-                <a className="level-item">
-                  <span
-                    onClick={this.onLikeClick.bind(this, post._id)}
-                    className="icon is-small"
-                  >
-                    <i
-                      className={classnames("fas fa-thumbs-up", {
-                        "has-text-success": this.findUserLike(post.likes)
-                      })}
-                    />{" "}
-                    {post.likes.length}{" "}
-                  </span>{" "}
-                </a>{" "}
-                <a className="level-item">
-                  <span
-                    onClick={this.onUnLikeClick.bind(this, post._id)}
-                    className="icon is-small"
-                  >
-                    <i className="fas fa-thumbs-down" />
-                  </span>{" "}
-                </a>{" "}
-              </div>{" "}
-            </nav>
+              </nav>
+            ) : null}{" "}
+          </div>{" "}
+          {post.user === auth.user.id ? (
+            <span
+              onClick={this.onDeleteClick.bind(this, post._id)}
+              className="icon is-small"
+            >
+              <i className="fas fa-times" />
+            </span>
           ) : null}{" "}
-        </div>{" "}
-        {post.user === auth.user.id ? (
-          <span
-            onClick={this.onDeleteClick.bind(this, post._id)}
-            className="icon is-small"
-          >
-            <i className="fas fa-times" />
-          </span>
-        ) : null}{" "}
-      </article>
+        </article>
+      </div>
     );
   }
 }

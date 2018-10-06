edf4af044ae028fe824f2b2ca1f214a12acb4a82
Yeondam Park <giyen9088@gmail.com>
2018-10-05 19:35:39 -0700

    adding content on landing page

--- client/src/components/Layout/Landing.js
+++ client/src/components/Layout/Landing.js
@@ -7,6 +7,7 @@ import icon1 from "../../images/offices.svg";
 import icon2 from "../../images/process.svg";
 import icon3 from "../../images/coding.svg";
 import bg from "../../images/devconnector-bg.jpg";
+import devObj from "../../images/devObj.png";
 
 class Landing extends Component {
   componentDidMount() {
@@ -17,7 +18,7 @@ class Landing extends Component {
   render() {
     return (
       <Fragment>
-        <section className="is-hero section">
+        <section className="section">
           <div
             className="main-bg"
             style={{
@@ -30,8 +31,25 @@ class Landing extends Component {
             <div className="columns is-vcentered">
               <div className="column is-12">
                 <div className="section-header">
-                  <h1 className="title is-spaced"> Devconnector </h1>{" "}
-                  <h2 className="subtitle is-3">
+                  <h1
+                    className="title is-spaced"
+                    style={{
+                      width: "320px",
+                      backgroundColor: "rgba(0,0,0,0.7)",
+                      padding: "10px"
+                    }}
+                  >
+                    {" "}
+                    Devconnector{" "}
+                  </h1>{" "}
+                  <h2
+                    className="subtitle is-3"
+                    style={{
+                      width: "620px",
+                      backgroundColor: "rgba(0,0,0,0.7)",
+                      padding: "10px"
+                    }}
+                  >
                     Social Network for Developer{" "}
                   </h2>{" "}
                   <div className="field is-grouped">
@@ -56,7 +74,10 @@ class Landing extends Component {
             </div>{" "}
           </div>{" "}
         </section>{" "}
-        <section className="is-medium has-background is-usp">
+        <section
+          className="is-medium has-background is-usp"
+          style={{ marginTop: "10rem" }}
+        >
           <div className="container">
             <div className="columns">
               <div className="column is-4">
@@ -119,6 +140,41 @@ class Landing extends Component {
             </div>{" "}
           </div>{" "}
         </section>{" "}
+        <section
+          className="container"
+          style={{ marginTop: "10rem", marginBottom: "10rem" }}
+        >
+          <div className="columns">
+            <div className="column is-8">
+              <img
+                src="http://source.unsplash.com/random/900x600"
+                alt="random bg"
+              />
+            </div>
+            <div className="column is-4">
+              <div className="content">
+                <h1>Devconnector</h1>
+                <p>Help us to connect to the world</p>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section
+          className="container"
+          style={{ marginTop: "10rem", marginBottom: "10rem" }}
+        >
+          <div className="columns">
+            <div className="column is-4">
+              <div className="content">
+                <h1>Devconnector</h1>
+                <p>Help us to connect to the world</p>
+              </div>
+            </div>
+            <div className="column is-8">
+              <img src={devObj} alt="random bg" />
+            </div>
+          </div>
+        </section>
       </Fragment>
     );
   }
